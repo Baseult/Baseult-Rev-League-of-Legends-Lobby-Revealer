8aaaa08852f072da5ad938a3e1bf1546e652b93e
Baseult - Discord Tools <45830921+Baseult@users.noreply.github.com>
2022-11-29 03:05:49 +0100

    Initial Upload

---
+++ GetSummonerNames/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using GetSummonerNames.Properties;
+
+namespace GetSummonerNames
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel4 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel5 = new System.Windows.Forms.LinkLabel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Location = new System.Drawing.Point(20, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "GET NAMES";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.Cyan;
+            this.button2.Location = new System.Drawing.Point(146, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "U.GG";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(65)))), ((int)(((byte)(65)))), ((int)(((byte)(65)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(272, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "DODGE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(21, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(360, 27);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Join a Lobby and click on \"Get Names\"...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.linkLabel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel1.Enabled = false;
+            this.linkLabel1.ForeColor = System.Drawing.Color.Cyan;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel1.Location = new System.Drawing.Point(27, 171);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(349, 21);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "PLAYER 1";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.linkLabel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel2.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel2.Enabled = false;
+            this.linkLabel2.ForeColor = System.Drawing.Color.Cyan;
+            this.linkLabel2.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel2.Location = new System.Drawing.Point(27, 203);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(349, 21);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "PLAYER 2";
+            this.linkLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.linkLabel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel3.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel3.Enabled = false;
+            this.linkLabel3.ForeColor = System.Drawing.Color.Cyan;
+            this.linkLabel3.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel3.Location = new System.Drawing.Point(27, 235);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(349, 21);
+            this.linkLabel3.TabIndex = 7;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "PLAYER 3";
+            this.linkLabel3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
+            // 
+            // linkLabel4
+            // 
+            this.linkLabel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.linkLabel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel4.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel4.Enabled = false;
+            this.linkLabel4.ForeColor = System.Drawing.Color.Cyan;
+            this.linkLabel4.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel4.Location = new System.Drawing.Point(27, 267);
+            this.linkLabel4.Name = "linkLabel4";
+            this.linkLabel4.Size = new System.Drawing.Size(349, 21);
+            this.linkLabel4.TabIndex = 8;
+            this.linkLabel4.TabStop = true;
+            this.linkLabel4.Text = "PLAYER 4";
+            this.linkLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel4.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel4_LinkClicked);
+            // 
+            // linkLabel5
+            // 
+            this.linkLabel5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.linkLabel5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.linkLabel5.DisabledLinkColor = System.Drawing.Color.DarkGray;
+            this.linkLabel5.Enabled = false;
+            this.linkLabel5.ForeColor = System.Drawing.Color.Cyan;
+            this.linkLabel5.LinkColor = System.Drawing.Color.Aqua;
+            this.linkLabel5.Location = new System.Drawing.Point(27, 299);
+            this.linkLabel5.Name = "linkLabel5";
+            this.linkLabel5.Size = new System.Drawing.Size(349, 21);
+            this.linkLabel5.TabIndex = 9;
+            this.linkLabel5.TabStop = true;
+            this.linkLabel5.Text = "PLAYER 5";
+            this.linkLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel5_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button4.Location = new System.Drawing.Point(20, 344);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 26);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "MINIMIZE";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Location = new System.Drawing.Point(212, 344);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(169, 26);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "CLOSE";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(400, 384);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.linkLabel5);
+            this.Controls.Add(this.linkLabel4);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lobby Names - Baseult";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Label label1;
+        private LinkLabel linkLabel1;
+        private LinkLabel linkLabel2;
+        private LinkLabel linkLabel3;
+        private LinkLabel linkLabel4;
+        private LinkLabel linkLabel5;
+        private Button button4;
+        private Button button5;
+    }
+}
+
---
+++ GetSummonerNames/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using GetSummonerNames.Properties;
+using Newtonsoft.Json;
+
+namespace GetSummonerNames
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class PlayerRegion
+        {
+            public string region { get; set; }
+        }
+
+        public class Player
+        {
+            public string Name { get; set; }
+        }
+
+        public class Players
+        {
+            public List<Player> Participants { get; set; }
+        }
+
+        public static Dictionary<int, string> PlayerList { get; set; } = new Dictionary<int, string>();
+        public static Dictionary<int, string> Linklist { get; set; } = new Dictionary<int, string>();
+        public static Dictionary<string, string> Riot { get; set; } = new Dictionary<string, string>();
+        public static Dictionary<string, string> Client { get; set; } = new Dictionary<string, string>();
+
+        private string _myregion;
+        private string _uggplayers;
+        private const string Mobalytics = "https://app.mobalytics.gg/lol/profile/";
+
+        public const int WmNclbuttondown = 0xA1;
+        public const int HtCaption = 0x2;
+
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int msg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WmNclbuttondown, HtCaption, 0);
+            }
+        }
+
+        private static string Cmd(string gamename)
+        {
+            var commandline = "";
+            var mngmtClass = new ManagementClass("Win32_Process");
+            foreach (var managementBaseObject in mngmtClass.GetInstances())
+            {
+                var o = (ManagementObject)managementBaseObject;
+                if (o["Name"].Equals(gamename))
+                {
+                    commandline = "[" + o["CommandLine"] + "]";
+                }
+            }
+
+            return commandline;
+        }
+
+        private static string Findstring(string text, string from, string to)
+        {
+            var pFrom = text.IndexOf(from, StringComparison.Ordinal) + from.Length;
+            var pTo = text.LastIndexOf(to, StringComparison.Ordinal);
+
+            return text.Substring(pFrom, pTo - pFrom);
+        }
+
+        private static string Getregion(string requeqst)
+        {
+            return JsonConvert.DeserializeObject<PlayerRegion>(requeqst).region;
+        }
+
+        private static void get_lcu()
+        {
+            Riot.Clear();
+            Client.Clear();
+
+            var commandline = Cmd("LeagueClientUx.exe");
+
+            Riot.Add("port", Findstring(commandline, "--riotclient-app-port=", "\" \"--no-rads"));
+            Riot.Add("token", Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("riot:" + Findstring(commandline, "--riotclient-auth-token=", "\" \"--riotclient"))));
+
+            Client.Add("port", Findstring(commandline, "--app-port=", "\" \"--install"));
+            Client.Add("token", Convert.ToBase64String(Encoding.GetEncoding("ISO-8859-1").GetBytes("riot:" + Findstring(commandline, "--remoting-auth-token=", "\" \"--respawn-command=LeagueClient.exe"))));
+        }
+
+        private static string MakeRequest(string type, string url, bool client)
+        {
+            try
+            {
+                ServicePointManager.ServerCertificateValidationCallback += delegate
+                {
+                    const bool validationResult = true;
+                    return validationResult;
+                };
+
+                int port;
+                string token;
+
+                if(client)
+                {
+                    port = Convert.ToInt32(Client["port"]);
+                    token = Client["token"];
+                }
+                else
+                {
+                    port = Convert.ToInt32(Riot["port"]);
+                    token = Riot["token"];
+                }
+
+                var request = (HttpWebRequest)WebRequest.Create("https://127.0.0.1:" + port + url);
+                request.PreAuthenticate = true;
+                request.ContentType = "application/json";
+                request.Method = type;
+                request.Headers.Add("Authorization", "Basic " + token);
+
+                var httpResponse = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Request failed - No Connection...", "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return "";
+            }
+        }
+
+        private void Resetlabel()
+        {
+            linkLabel1.Enabled = false;
+            linkLabel2.Enabled = false;
+            linkLabel3.Enabled = false;
+            linkLabel4.Enabled = false;
+            linkLabel5.Enabled = false;
+
+            linkLabel1.Text = "PLAYER 1";
+            linkLabel2.Text = "PLAYER 2";
+            linkLabel3.Text = "PLAYER 3";
+            linkLabel4.Text = "PLAYER 4";
+            linkLabel5.Text = "PLAYER 5";
+        }
+
+
+        private void Getplayers(string req)
+        {
+            Linklist.Clear();
+            PlayerList.Clear();
+            _uggplayers = "";
+            var deserialized = JsonConvert.DeserializeObject<Players>(req);
+            var count = 0;
+
+            foreach (var player in deserialized.Participants)
+            {
+                count++;
+                PlayerList.Add(count, player.Name);
+                Console.WriteLine(player.Name);
+                Linklist.Add(count, Mobalytics + _myregion + "/" + player.Name + "/overview");
+                _uggplayers += player.Name + ", ";
+            }
+
+            if (PlayerList.Count >= 1)
+            {
+                linkLabel1.Text = PlayerList[1];
+                linkLabel1.Enabled = true;
+
+                label1.Text = "Found Players in Lobby...";
+                BackgroundImage = Resources.on;
+                button2.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "No Players found...";
+                BackgroundImage = Resources.off;
+                button2.Enabled = false;
+            }
+
+            if (PlayerList.Count >= 2)
+            {
+                linkLabel2.Text = PlayerList[2];
+                linkLabel2.Enabled = true;
+            }
+
+            if (PlayerList.Count >= 3)
+            {
+                linkLabel3.Text = PlayerList[3];
+                linkLabel3.Enabled = true;
+            }
+
+            if (PlayerList.Count >= 4)
+            {
+                linkLabel4.Text = PlayerList[4];
+                linkLabel4.Enabled = true;
+            }
+
+            if (PlayerList.Count >= 5)
+            {
+                linkLabel5.Text = PlayerList[5];
+                linkLabel5.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Connecting to LCU...";
+            get_lcu();
+            label1.Text = "Searching for Players...";
+            _myregion = Getregion(MakeRequest("GET", "/riotclient/get_region_locale" /*Public Riot API request*/, true));
+            Getplayers(MakeRequest( "GET", "/chat/v5/participants/champ-select" /*Found Request in various Logs C:\Riot Games\League of Legends\Logs\LeagueClient*/, false));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Opening U.GG...";
+            Process.Start("https://u.gg/multisearch?summoners=" + _uggplayers + "&region=" + _myregion.ToLower() + "1");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var dialogResult = MessageBox.Show("You might get a Dodge Penalty!\r\nYou also lose LP if you dodge in Ranked.\r\nDo you still want to dodge?", "Are you sure?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                button2.Enabled = false;
+                label1.Text = "Dodging Lobby...";
+                get_lcu();
+                MakeRequest( "POST", "/lol-login/v1/session/invoke?destination=lcdsServiceProxy&method=call&args=[\"\",\"teambuilder-draft\",\"quitV2\",\"\"]", true); //Updated Dodge Request - Original Credits to "mfro - LeagueClient": https://github.com/mfro/LeagueClient/blob/95c403bd582713c420090dec4f63dae284ff6598/RiotClient/RiotServices.cs#L1092 - Updated with "KebsCS KBotExt": https://github.com/KebsCS/KBotExt/blob/94d13918558799e7704bd9fa50505362cdc7d47f/KBotExt/GameTab.h#L313
+                label1.Text = "Dodged Lobby...";
+
+                Resetlabel();
+                BackgroundImage = Resources.off;
+            }
+
+        }
+
+
+        private static readonly Random Random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[Random.Next(s.Length)]).ToArray());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var dialogResult = MessageBox.Show("Hey, there might be an update for my program.\r\n\r\nIf there is, you can download it from Unknowncheats or my Discord. To check out for a possible update and get redirected to UnknownCheats, press Yes.\r\n\r\n(If you obtained this program from a source other than UnknownCheats or my Discord server, it was not uploaded by me.)", "Baseult-Rev Information!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dialogResult == DialogResult.Yes)
+            {
+                Process.Start("https://www.unknowncheats.me/forum/league-of-legends/523020-ranked-12-22-reveal-teammates-lobby.html");
+            }
+
+            Text = RandomString(16);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Linklist[1]);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Linklist[2]);
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Linklist[3]);
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Linklist[4]);
+        }
+
+        private void linkLabel5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(Linklist[5]);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+
+}
